d3ddb1decbb82355e7118d57623d68f479d66ff1
madhuri <62393315+madhuri-bh@users.noreply.github.com>
2020-09-23 17:17:42 +0530

    Add files via upload

---
+++ Python2.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+n = input("Enter total number of gloves")
+
+print("Enter colors of array")
+  
+ 
+ar = list(map(int,input().split()))
+
+pairs = 0
+
+n = Counter(ar)
+for i in n.values():
+    
+pairs += (i//2)
+         
+
+print(pairs)
